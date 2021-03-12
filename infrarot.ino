bc2a03ece32dc41b486adc2d91a63c8e394269fe
bigdickdust <80242720+bigdickdust@users.noreply.github.com>
2021-03-12 11:33:15 +0100

    Create infrarot.ino

---
+++ infrarot.ino
@@ -0,0 +1,46 @@
+
+const int IR_INPUT_PIN = A0;
+const int IR_LED_PIN = 13;
+void setup(){
+    Serial.begin(9600); 
+    pinMode(IR_INPUT_PIN, INPUT);
+    pinMode(IR_LED_PIN, OUTPUT);
+
+}
+
+
+void loop(){
+  int ambient = 0;
+  int lit = 0;
+  int value = 0;
+  
+     digitalWrite(IR_LED_PIN, LOW);
+     delay(5);                           
+     ambient = analogRead(IR_INPUT_PIN);
+  
+     digitalWrite(IR_LED_PIN, HIGH);
+     delay(5);
+     lit = analogRead(IR_INPUT_PIN);
+  
+     value = lit - ambient;
+  
+     Serial.println(value);
+}
+void initRPR220(int ledPin, int detectorPin){
+  
+     pinMode(detectorPin, INPUT);
+     pinMode(ledPin, OUTPUT);
+}
+
+int readRPR220(int ledPin, int detectorPin){
+int val = 0;
+  
+     digitalWrite(ledPin, LOW);
+     delay(5);
+     val = analogRead(detectorPin);
+  
+     digitalWrite(ledPin, HIGH);
+     delay(5);
+     val = analogRead(detectorPin) - val;
+return 9000/(val - 12);
+}
